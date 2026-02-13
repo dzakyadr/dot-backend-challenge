ce06bc4df9c49e6a07ebe12667be3b29c91af441
unknown <muhadrian061@gmail.com>
2026-02-13 23:49:52 +0700

    feat: implement login with JWT token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('auth') // URL: http://localhost:3000/auth
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login') // URL: http://localhost:3000/auth/login
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    // 1. Cari user berdasarkan email
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('Email atau password salah');
+    }
+
+    // 2. Bandingkan password input dengan hashedPassword di DB
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Email atau password salah');
+    }
+
+    // 3. Kalau OK, buatkan Token JWT
+    const payload = { sub: user.id, email: user.email };
+    return {
+      message: 'Login berhasil',
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
